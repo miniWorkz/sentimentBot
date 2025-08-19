72b7de515342967a5fc85e097187718c537f39d6
armandohdzc <ahern110@charlotte.edu>
2025-08-19 12:55:28 -0400

    Fixed conditional and added goodbye

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ while True:
         print("I'm sorry to hear that. Would you like to talk about it?")
     if blob.sentiment.polarity > 0.5:
         print("I'm happy to hear that, please tell me more!")
-    print(blob.sentiment)
-    if user_input == "exit" or "goodbye" or "bye":
+    # print(blob.sentiment)                                      only used to tell the emotional score of the input
+    if user_input == "exit" or user_input == "goodbye" or user_input == "bye":
+        print("Goodbye, have a wonderful day!")
         break
     if user_input not in rules.rules:
         print("Sorry, I don't understand that.")
 
-    response = handle_rule_check(user_input, rules.rules)
-    print(response)
+    #response = handle_rule_check(user_input, rules.rules)
+    #print(response)
